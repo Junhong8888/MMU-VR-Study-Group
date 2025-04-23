e1e3f19bc0c12e71a0f98e813241bc6662e4b068
Junhong8888 <lowjunhong8888@gamil.com>
2025-04-24 01:23:30 +0800

    .

---
+++ users/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render , redirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+from .forms import SignUpForm
+
+# Create your views here.
+
+
+def home(request):
+    signup_form = SignUpForm()  
+
+    if request.method == 'POST':
+        form_type = request.POST.get('form_type')
+
+        if form_type == 'login':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('main')  
+            else:
+                messages.error(request, 'Invalid username or password.')
+
+        elif form_type == 'signup':
+            signup_form = SignUpForm(request.POST)
+            if signup_form.is_valid():
+                signup_form.save()
+                messages.success(request, 'Account created successfully. Please log in.')
+                return redirect('home')  
+            else:
+                print(signup_form.errors)
+
+    return render(request, 'mmu proj.html', {'form': signup_form})
+
+def main(request):
+    return render(request,'main.html')
+
+
